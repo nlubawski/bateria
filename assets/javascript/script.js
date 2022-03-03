d06b31d5ad39d73e129323c8156ff3d5413dea33
Nathan Lubawski <nathanlubawski1@gmail.com>
2022-03-03 11:47:13 -0300

    feat: addEventListener click and create function playComposition

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,11 +2,21 @@ document.body.addEventListener('keyup', (event) => {
     playSound(event.code.toLowerCase())
 })
 
+document.querySelector('.composer button').addEventListener('click', ()=>{
+    let song = document.querySelector('.composer input').value
+    
+    if (song !== ''){
+        let songArray = song.split('')
+        playComposition(songArray)
+    }
+})
+
 function playSound(sound) {
     let audioElement = document.querySelector(`#s_${sound}`)
     let keyElement = document.querySelector(`div[data-key="${sound}"]`)
 
     if(audioElement){
+        audioElement.currentTime = 0
         audioElement.play()
     }
 
@@ -17,5 +27,18 @@ function playSound(sound) {
             keyElement.classList.remove('active')
         }, 300)
     }
+}
 
+function playComposition(songArray){
+    let wait = 0
+
+    for(let songItem of songArray) {
+        setTimeout(()=>{
+            playSound(`key${songItem}`)
+        }, wait)
+        
+        wait += 250
+
+       
+    }
 }
